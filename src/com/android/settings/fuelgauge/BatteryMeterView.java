3b92be99b6a187c74da9c14abe2d81e92d156551
mobxCode <yovie079@gmail.com>
2021-11-25 13:03:16 +0700

    merge: init package settings

---
+++ src/com/android/settings/fuelgauge/BatteryMeterView.java
@@ -0,0 +1,242 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fuelgauge;
+
+import android.annotation.Nullable;
+import android.content.Context;
+import android.graphics.ColorFilter;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import androidx.annotation.VisibleForTesting;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settingslib.graph.CircleBatteryDrawable;
+import com.android.settingslib.graph.FullCircleBatteryDrawable;
+import com.android.settingslib.graph.ThemedBatteryDrawable;
+
+public class BatteryMeterView extends ImageView {
+    @VisibleForTesting
+    BatteryMeterDrawable mThemedDrawable;
+    @VisibleForTesting
+    ColorFilter mErrorColorFilter;
+    @VisibleForTesting
+    ColorFilter mAccentColorFilter;
+    @VisibleForTesting
+    ColorFilter mForegroundColorFilter;
+
+    CircleBatteryDrawable mCircleDrawable;
+    FullCircleBatteryDrawable mFullCircleDrawable;
+    private int mIconStyle = 0;
+
+    public BatteryMeterView(Context context) {
+        this(context, null, 0);
+    }
+
+    public BatteryMeterView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BatteryMeterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        final int frameColor = context.getColor(R.color.meter_background_color);
+        mAccentColorFilter = Utils.getAlphaInvariantColorFilterForColor(
+                Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent));
+        mErrorColorFilter = Utils.getAlphaInvariantColorFilterForColor(
+                context.getColor(R.color.battery_icon_color_error));
+        mForegroundColorFilter = Utils.getAlphaInvariantColorFilterForColor(
+                Utils.getColorAttrDefaultColor(context, android.R.attr.colorForeground));
+        mThemedDrawable = new BatteryMeterDrawable(context, frameColor);
+        mCircleDrawable = new CircleBatteryDrawable(context, frameColor);
+        mFullCircleDrawable = new FullCircleBatteryDrawable(context, frameColor);
+    }
+
+    public void setDrawableStyle() {
+        int style = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        switch (style) {
+            case 1:
+            case 2:
+                mCircleDrawable.setMeterStyle(style);
+                mCircleDrawable.setColorFilter(mAccentColorFilter);
+                setImageDrawable(mCircleDrawable);
+                break;
+            case 3:
+                mFullCircleDrawable.setColorFilter(mAccentColorFilter);
+                setImageDrawable(mFullCircleDrawable);
+                break;
+            default:
+                mThemedDrawable.setColorFilter(mAccentColorFilter);
+                setImageDrawable(mThemedDrawable);
+        }
+        if (mIconStyle != style) {
+            mIconStyle = style;
+            postInvalidate();
+        }
+    }
+
+    public void setBatteryLevel(int level) {
+        mCircleDrawable.setBatteryLevel(level);
+        mFullCircleDrawable.setBatteryLevel(level);
+        mThemedDrawable.setBatteryLevel(level);
+        updateColorFilter();
+    }
+
+    public int getBatteryLevel() {
+        return mThemedDrawable.getBatteryLevel();
+    }
+
+    public void setPowerSave(boolean powerSave) {
+        mCircleDrawable.setPowerSaveEnabled(powerSave);
+        mFullCircleDrawable.setPowerSaveEnabled(powerSave);
+        mThemedDrawable.setPowerSaveEnabled(powerSave);
+        updateColorFilter();
+    }
+
+    public boolean getPowerSave() {
+        return mThemedDrawable.getPowerSaveEnabled();
+    }
+
+    public void setCharging(boolean charging) {
+        mCircleDrawable.setCharging(charging);
+        mFullCircleDrawable.setCharging(charging);
+        mThemedDrawable.setCharging(charging);
+        postInvalidate();
+    }
+
+    public boolean getCharging() {
+        return mThemedDrawable.getCharging();
+    }
+
+    private int getCriticalLevel() {
+        return mThemedDrawable.getCriticalLevel();
+    }
+
+    private void updateColorFilter() {
+        final boolean powerSaveEnabled = getPowerSave();
+        final int level = getBatteryLevel();
+        if (powerSaveEnabled) {
+            mCircleDrawable.setColorFilter(mForegroundColorFilter);
+            mFullCircleDrawable.setColorFilter(mForegroundColorFilter);
+            mThemedDrawable.setColorFilter(mForegroundColorFilter);
+        } else if (level < getCriticalLevel()) {
+            mCircleDrawable.setColorFilter(mErrorColorFilter);
+            mFullCircleDrawable.setColorFilter(mErrorColorFilter);
+            mThemedDrawable.setColorFilter(mErrorColorFilter);
+        } else {
+            mCircleDrawable.setColorFilter(mAccentColorFilter);
+            mFullCircleDrawable.setColorFilter(mAccentColorFilter);
+            mThemedDrawable.setColorFilter(mAccentColorFilter);
+        }
+    }
+
+    public static class BatteryMeterDrawable extends ThemedBatteryDrawable {
+        private final int mIntrinsicWidth;
+        private final int mIntrinsicHeight;
+
+        public BatteryMeterDrawable(Context context, int frameColor) {
+            super(context, frameColor);
+
+            mIntrinsicWidth = context.getResources()
+                    .getDimensionPixelSize(R.dimen.battery_meter_width);
+            mIntrinsicHeight = context.getResources()
+                    .getDimensionPixelSize(R.dimen.battery_meter_height);
+        }
+
+        public BatteryMeterDrawable(Context context, int frameColor, int width, int height) {
+            super(context, frameColor);
+
+            mIntrinsicWidth = width;
+            mIntrinsicHeight = height;
+        }
+
+        @Override
+        public int getIntrinsicWidth() {
+            return mIntrinsicWidth;
+        }
+
+        @Override
+        public int getIntrinsicHeight() {
+            return mIntrinsicHeight;
+        }
+    }
+
+    public static class CircleBatteryMeterDrawable extends CircleBatteryDrawable {
+        private final int mIntrinsicWidth;
+        private final int mIntrinsicHeight;
+
+        public CircleBatteryMeterDrawable(Context context, int frameColor) {
+            super(context, frameColor);
+
+            mIntrinsicWidth = context.getResources()
+                    .getDimensionPixelSize(R.dimen.battery_meter_width);
+            mIntrinsicHeight = context.getResources()
+                    .getDimensionPixelSize(R.dimen.battery_meter_height);
+        }
+
+        public CircleBatteryMeterDrawable(Context context, int frameColor, int width, int height) {
+            super(context, frameColor);
+
+            mIntrinsicWidth = width;
+            mIntrinsicHeight = height;
+        }
+
+        @Override
+        public int getIntrinsicWidth() {
+            return mIntrinsicWidth;
+        }
+
+        @Override
+        public int getIntrinsicHeight() {
+            return mIntrinsicHeight;
+        }
+    }
+
+    public static class FullCircleBatteryMeterDrawable extends FullCircleBatteryDrawable {
+        private final int mIntrinsicWidth;
+        private final int mIntrinsicHeight;
+
+        public FullCircleBatteryMeterDrawable(Context context, int frameColor) {
+            super(context, frameColor);
+
+            mIntrinsicWidth = context.getResources()
+                    .getDimensionPixelSize(R.dimen.battery_meter_width);
+            mIntrinsicHeight = context.getResources()
+                    .getDimensionPixelSize(R.dimen.battery_meter_height);
+        }
+
+        public FullCircleBatteryMeterDrawable(Context context, int frameColor, int width, int height) {
+            super(context, frameColor);
+
+            mIntrinsicWidth = width;
+            mIntrinsicHeight = height;
+        }
+
+        @Override
+        public int getIntrinsicWidth() {
+            return mIntrinsicWidth;
+        }
+
+        @Override
+        public int getIntrinsicHeight() {
+            return mIntrinsicHeight;
+        }
+    }
+}
---
+++ src/com/android/settings/gestures/SwipeToNotificationPreferenceController.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import static android.provider.Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS;
+import static android.provider.Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.widget.VideoPreference;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnPause;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+
+public class SwipeToNotificationPreferenceController extends BasePreferenceController
+        implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener,
+        LifecycleObserver, OnResume, OnPause {
+
+    private static final int ON = 1;
+    private static final int OFF = 0;
+
+    private static final String PREF_KEY_VIDEO = "gesture_swipe_down_fingerprint_video";
+    private static final String PREF_SWIPE_DOWN = "gesture_swipe_down_fingerprint";
+    private static final String PREF_SWIPE_DISMISS = "gesture_swipe_dismiss_fingerprint";
+
+    private PreferenceScreen mPreferenceScreen;
+    private VideoPreference mVideoPreference;
+
+    public SwipeToNotificationPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
+        return !isGestureAvailable(context)
+                || prefs.getBoolean(SwipeToNotificationSettings.PREF_KEY_SUGGESTION_COMPLETE,
+                false);
+    }
+
+    private static boolean isGestureAvailable(Context context) {
+        return Utils.hasFingerprintHardware(context)
+                && context.getResources()
+                .getBoolean(com.android.internal.R.bool.config_supportSystemNavigationKeys);
+    }
+
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        if (isAvailable()) {
+            mVideoPreference = (VideoPreference) screen.findPreference(PREF_KEY_VIDEO);
+        }
+        mPreferenceScreen = screen;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return isAvailable(mContext) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    /*@Override
+    public boolean isSliceable() {
+        return TextUtils.equals(getPreferenceKey(), "gesture_swipe_down_fingerprint");
+    }*/
+
+    /*@Override
+    public boolean isPublicSlice() {
+        return true;
+    }*/
+
+    @Override
+    public void onPause() {
+        if (mVideoPreference != null) {
+            mVideoPreference.onViewInvisible();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        if (mVideoPreference != null) {
+            mVideoPreference.onViewVisible();
+        }
+    }
+
+    public static boolean isAvailable(Context context) {
+        return isGestureAvailable(context);
+    }
+
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        if (preference != null && preference instanceof SwitchPreference) {
+            SwitchPreference pref = (SwitchPreference) preference;
+            if (TextUtils.equals(pref.getKey(), PREF_SWIPE_DOWN)) {
+                boolean enabled = Settings.Secure.getInt(
+                        mContext.getContentResolver(),
+                        SYSTEM_NAVIGATION_KEYS_ENABLED, ON) == ON;
+                pref.setChecked(enabled);
+            } else if (TextUtils.equals(pref.getKey(), PREF_SWIPE_DISMISS)) {
+                boolean enabled = Settings.Secure.getInt(
+                        mContext.getContentResolver(),
+                        FP_SWIPE_TO_DISMISS_NOTIFICATIONS, ON) == ON;
+                pref.setChecked(enabled);
+                pref.setEnabled(swipeDownEnabled());
+            }
+        }
+    }
+
+    private boolean swipeDownEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                SYSTEM_NAVIGATION_KEYS_ENABLED, ON) == ON;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        SwitchPreference pref = (SwitchPreference) preference;
+        if (TextUtils.equals(pref.getKey(), PREF_SWIPE_DOWN)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    SYSTEM_NAVIGATION_KEYS_ENABLED, enabled ? ON : OFF);
+            pref.setChecked(enabled);
+            SwitchPreference dismissPref = (SwitchPreference) mPreferenceScreen.findPreference(PREF_SWIPE_DISMISS);
+            if (dismissPref != null) {
+                dismissPref.setEnabled(enabled);
+            }
+        } else if (TextUtils.equals(pref.getKey(), PREF_SWIPE_DISMISS)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                    FP_SWIPE_TO_DISMISS_NOTIFICATIONS, enabled ? ON : OFF);
+            pref.setChecked(enabled);
+        }
+        return true;
+    }
+}
